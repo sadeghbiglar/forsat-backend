556dee5ec7ef419c1ed397e59c85a2cebcb7d67e
sadeghbiglar <sbmail555@gmail.com>
2022-07-31 07:31:07 -0700

    Create Models & Migrations & Controllers

---
+++ app/Http/Controllers/OpportunityDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OpportunityDetail;
+use Illuminate\Http\Request;
+
+class OpportunityDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OpportunityDetail  $opportunityDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OpportunityDetail $opportunityDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OpportunityDetail  $opportunityDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OpportunityDetail $opportunityDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OpportunityDetail  $opportunityDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OpportunityDetail $opportunityDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OpportunityDetail  $opportunityDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OpportunityDetail $opportunityDetail)
+    {
+        //
+    }
+}
---
+++ app/Models/Lookups/Country.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Lookups;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+}
