14a0d772bcbaa9a34ace3c38465d16a8f98ac882
sadeghbiglar <sbmail555@gmail.com>
2022-07-31 08:03:22 -0700

    Create Country seed part5-2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategorySeeder::class);
+        $this->call(CountrySeeder::class);
 
         // \App\Models\User::factory(10)->create();
 
